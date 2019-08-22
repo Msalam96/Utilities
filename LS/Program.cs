de5686e7b321316980f1453a3d763ff4a8ee74e9
Mohammed Salam <mohammed.salam912@myhunter.cuny.edu>
2019-08-22 09:34:05 -0400

    Pushing basic LS function

---
+++ LS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = Directory.GetCurrentDirectory();
+            //Console.Write(path);
+            //Console.ReadKey();
+            DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            foreach (DirectoryInfo directoryInfo in directory.GetDirectories())
+            {
+                string file = directoryInfo.Name;
+                file += "/";
+                Console.WriteLine(file);
+                //Console.WriteLine(@"{0, -30}\ directory", d.Name);
+            }
+
+            foreach (FileInfo fileInfo in directory.GetFiles())
+            {
+                Console.WriteLine(fileInfo.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
